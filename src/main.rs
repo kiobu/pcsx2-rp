ac0d636b797a98ee54e33a6d9e948c6c3d47d39c
kiobu <cdolson30@gmail.com>
2020-11-16 18:06:46 -0500

    Begin parsing emuLog.txt; new bug: The filename, directory name, or volume label syntax is incorrect.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::path::{PathBuf};
 use std::env;
 use serde_json::{Value};
 
-pub fn read_config() -> Value {
+fn read_config() -> Value {
     let cdir = match env::current_dir() {
         Ok(pathbuf) => { pathbuf },
         Err(_) => panic!("No buffer.")
@@ -13,9 +14,28 @@ pub fn read_config() -> Value {
 
     let path = cdir.join(PathBuf::from("config.json"));
 
-    println!("{:?}", &path);
-
     let file = File::open(path)
+        .expect("Could not open config.");
+    let mut buffer = BufReader::new(file);
+    let mut result = String::new();
+
+    let _: usize = match buffer.read_to_string(&mut result) {
+        Ok(_n) => _n,
+        Err(_) => 0
+    };
+    return match serde_json::from_str(&result[0..]) {
+        Ok(obj) => { obj },
+        Err(_) => panic!("Could not parse JSON.")
+    };
+}
+
+fn read_file(npath: String) -> String {
+
+    let path = npath.replace("/", "\\");
+
+    println!("{}", path);
+
+    let file = File::open(PathBuf::from(String::from(path.trim())))
         .expect("Could not open file.");
     let mut buffer = BufReader::new(file);
     let mut result = String::new();
@@ -25,14 +45,34 @@ pub fn read_config() -> Value {
         Err(_) => 0
     };
 
-    return match serde_json::from_str(&result[0..]) {
-        Ok(obj) => { obj },
-        Err(_) => panic!("Could not parse JSON.")
-    };
+    return result;
+}
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    write!(stdout, "").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
 fn main() {
     let path = &read_config()["path"];
 
-    println!("{}", path);
+    if path == "" {
+        println!(
+        "Your path has not been set up! Make sure the path to your emuLog.txt is set in the config.json file. It should look like this: {}",
+        "
+        {
+            \"path\": \"C:/my/path/emuLog.txt\"   
+        }
+        "
+        );
+        pause();
+    } else {
+        let result = read_file(path.to_string());
+    }
 }
